98e3cca35078e7a19d59322e6d23495e61b0a840
Godstime315 <97334195+Godstime315@users.noreply.github.com>
2024-07-06 08:56:53 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -70,12 +70,12 @@ async def start(client, message):
             logger.error("MAKE SURE BOT IS ADMIN IN FORCESUB CHANNEL")
             return
         btn = [
-            [
+            [            
                 InlineKeyboardButton(
-                    "JԾIN 1", url=invite_link.invite_link
+                    "JԾIN 1", url=CHNL_LNK
                 ),
                 InlineKeyboardButton(
-                    "JԾIN 2", url=CHNL_LNK
+                    "JԾIN 2", url=invite_link.invite_link
                 )
             ]
         ]
