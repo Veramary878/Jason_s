963f627f90ce636b6ef330705079681304c42f72
Godstime315 <97334195+Godstime315@users.noreply.github.com>
2024-07-06 04:39:11 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -89,7 +89,7 @@ async def start(client, message):
                 btn.append([InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
-            text="**YOU ARE NOT IN OUR UPDATE CHANNELS GIVEN BELOW, SO YOU WON'T GET THE MOVIE FILE...\n\nIF YOU WANT THE MOVIE FILE, JOIN\nt.me/Lordshipmovies &\nt.me/Hislordship_Movies\n\nTHEN CLICK ON THE '↻ TRY AGAIN' BUTTON BELOW...\n\nTHEN YOU WILL GET THE MOVIE FILES...**",
+            text="**You need to join my Channel in order to get movie files t.me/Lordshipmovies\n\nKindly Please join the 2 Channels below**",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.MARKDOWN
             )
